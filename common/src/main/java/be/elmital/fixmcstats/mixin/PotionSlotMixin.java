1eb354d20da085e07c39b0d4407d5d544b549f8a
404Setup <153366651+404Setup@users.noreply.github.com>
2025-05-19 15:27:04 +0800

    Fixed PotionSlot not found

--- common/src/main/java/be/elmital/fixmcstats/mixin/PotionSlotMixin.java
+++ common/src/main/java/be/elmital/fixmcstats/mixin/PotionSlotMixin.java
@@ -2,12 +2,13 @@ package be.elmital.fixmcstats.mixin;
 
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.BrewingStandMenu;
 import net.minecraft.world.inventory.Slot;
 import net.minecraft.world.item.ItemStack;
 import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
 
-@Mixin(targets = "net.minecraft.world.inventory.BrewingStandMenu.PotionSlot")
+@Mixin(BrewingStandMenu.PotionSlot.class)
 public class PotionSlotMixin extends Slot {
     // Fix https://bugs.mojang.com/browse/MC-154487
     public PotionSlotMixin(Inventory inventory, int index, int x, int y) {
